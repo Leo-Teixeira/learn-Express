63546124f1c2b1c5e399179b4571f2b7f8b195b7
Leo Teixeira <teixeiraleo72@gmail.com>
2023-03-16 06:05:49 +0100

    mise en place de ma premiere route avec variables

--- public/routes/adminRoutes.js
+++ public/routes/adminRoutes.js
@@ -5,6 +5,18 @@ router.get("/connect", function (req, res) {
   res.render("connect.ejs");
 });
 
+router.post("/connect", (req, res) => {
+  const login = req.body.login;
+  const password = req.body.password;
+
+  if (login === "admin" && password === "admin") {
+    req.session.login = login;
+    res.redirect("/404");
+  } else {
+    res.render("connexion.ejs", { message: "Identifiants incorrects" });
+  }
+});
+
 router.get("/deconnect", function (req, res) {
   res.render("404.ejs");
 });
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import session from "express-session";
+// import ejs from "ejs";
 import http from "http";
 import { Server } from "socket.io";
 import path from "path";
@@ -16,13 +18,19 @@ app.set("view engine", "ejs");
 import auth from "./public/routes/adminRoutes.js";
 import menu from "./public/routes/menuRoutes.js";
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "clef_secrete",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.use(express.static(__dirname + "/public"));
 app.use(auth);
 app.use(menu);
-
-app.get("/", (req, res) => {
-  res.render("connect");
-});
 
 app.get("/error", (req, res) => {
   res.render("404");
